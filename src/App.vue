ab4e6d8f282f9d12bd77d6bf11eb4298cef4ccc5
zhangyue <zhangyuetmac@163.com>
2023-12-12 15:51:29 +0800

    vue3初始化

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,88 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <el-row>
+    <el-col :span="24">
+      {{ isTrue }}
+      <el-table :data="state.tableData" style="width: 100%">
+        <el-table-column fixed prop="date" label="Date" width="150" />
+        <el-table-column prop="name" label="Name" width="120" />
+        <el-table-column prop="state" label="State" width="120" />
+        <el-table-column prop="city" label="City" width="120" />
+        <el-table-column prop="address" label="Address" width="600" />
+        <el-table-column prop="zip" label="Zip" width="120" />
+        <el-table-column fixed="right" label="Operations" width="120">
+          <template #default="scope">
+            <el-button link type="primary" size="small" @click="handleClick(scope.row)">Detail</el-button>
+            <el-button link type="primary" size="small">Edit</el-button>
+          </template>
+        </el-table-column>
+      </el-table>
+    </el-col>
+  </el-row>
 </template>
 
-<script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+<script setup>
+import { ref, reactive } from "vue";
+const isTrue = ref("11");
+const state = reactive({
+  tableData: [
+    {
+      date: "2016-05-03",
+      name: "Tom",
+      state: "California",
+      city: "Los Angeles",
+      address: "No. 189, Grove St, Los Angeles",
+      zip: "CA 90036",
+      tag: "Home"
+    },
+    {
+      date: "2016-05-02",
+      name: "Tom",
+      state: "California",
+      city: "Los Angeles",
+      address: "No. 189, Grove St, Los Angeles",
+      zip: "CA 90036",
+      tag: "Office"
+    },
+    {
+      date: "2016-05-04",
+      name: "Tom",
+      state: "California",
+      city: "Los Angeles",
+      address: "No. 189, Grove St, Los Angeles",
+      zip: "CA 90036",
+      tag: "Home"
+    },
+    {
+      date: "2016-05-01",
+      name: "Tom",
+      state: "California",
+      city: "Los Angeles",
+      address: "No. 189, Grove St, Los Angeles",
+      zip: "CA 90036",
+      tag: "Office"
+    }
+  ]
+});
+const handleClick = (item) => {
+  console.log("item", item);
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+  background: red;
 }
 </style>
